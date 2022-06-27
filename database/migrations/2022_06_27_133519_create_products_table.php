1a4843f1d2d0bfd05c66529606b88c8100081bfa
Charley Le Berre <c.leberre@hotmail.fr>
2022-06-27 15:59:10 +0200

    Ajout table Product + relation avec Category

---
+++ database/migrations/2022_06_27_133519_create_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 254);
+            $table->mediumText('description');
+            $table->decimal('prix', 5, 2);
+            $table->float('quantite');
+            $table->boolean('ahead')->nullable();
+            $table->boolean('active')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
