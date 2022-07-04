9dc73584ce1b3677f3e5c9f8fad406791659b5a1
Cleberre22 <c.leberre@hotmail.fr>
2022-07-04 16:49:20 +0200

    Update layout.blade.php

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -12,82 +12,39 @@
 <body>
 
     <header class="d-flex justify-content-between align-items-center p-3 text-white bg-dark">
-
-        <nav class="navbar navbar-expand-sm navbar-dark bg-dark" aria-label="Third navbar example">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="{{ route('home.index') }}">Boutique Laravel</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample03" aria-controls="navbarsExample03" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarsExample03">
-                    <ul class="navbar-nav me-auto mb-2 mb-sm-0">
-                        
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ url('/products') }}">Produits</a>
-                        </li>
-                        @auth
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" id="dropdown03" data-bs-toggle="dropdown" aria-expanded="false">{{ Auth::user()->name }}</a>
-                            <ul class="dropdown-menu" aria-labelledby="dropdown03">
-                                <form method="POST" action="{{ route('logout') }}">
-                                    @csrf
-                                    <x-dropdown-link :href="route('logout')" onclick="event.preventDefault();
-                                                this.closest('form').submit();">
-                                        {{ __('Log Out') }}
-                                    </x-dropdown-link>
-                                </form>
-                                <li><a class="dropdown-item" method="POST" href="{{ url('logout') }}">Se déconnecter</a></li>
-                            </ul>
-                        </li>
-                        @endauth
-                    </ul>
-                </div>
-            </div>
-        </nav>
-
-        @if (Route::has('login'))
-        <div class="hidden fixed">
-
+        <nav class="container d-flex flex-column flex-md-row justify-content-between text-white">
+            <a class="py-2" href="{{ route('home.index') }}" aria-label="Product">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="d-block mx-auto" role="img" viewBox="0 0 24 24">
+                    <title>Accueil</title>
+                    <circle cx="12" cy="12" r="10" />
+                    <path d="M14.31 8l5.74 9.94M9.69 8h11.48M7.38 12l5.74-9.94M9.69 16L3.95 6.06M14.31 16H2.83m13.79-4l-5.74 9.94" />
+                </svg>
+            </a>
+            <a class="py-2 d-none d-md-inline-block" href="#">Tour</a>
+            <a class="py-2 d-none d-md-inline-block" href="{{ url('/products') }}">Produits</a>
+            <a class="py-2 d-none d-md-inline-block" href="#">Features</a>
+            <a class="py-2 d-none d-md-inline-block" href="#">Enterprise</a>
+            <a class="py-2 d-none d-md-inline-block" href="#">Support</a>
             @auth
             @if (Auth::user()->is_admin === 1)
-            <a href="{{ url('/admin/index') }}" class="text-sm text-white text-decoration-none">Dashboard</a>
+            <a class="py-2 d-none d-md-inline-block" href="{{ url('/admin/index') }}">Dashboard</a>
             @endif
             @endauth
-
             @auth
-            <form method="POST" action="{{ route('logout') }}">
-                                    @csrf
-                                    <x-dropdown-link :href="route('logout')" onclick="event.preventDefault();
+            <form class="py-2 d-none d-md-inline-block" method="POST" action="{{ route('logout') }}">
+                @csrf
+                <x-dropdown-link :href="route('logout')" onclick="event.preventDefault();
                                                 this.closest('form').submit();">
-                                        {{ __('Log Out') }}
-                                    </x-dropdown-link>
-                                </form>
+                    {{ __('Déconnexion') }}
+                </x-dropdown-link>
+            </form>
             @else
-            <a href="{{ route('login') }}" class="text-sm text-white text-decoration-none">Connexion</a>
-
+            <a class="py-2 d-none d-md-inline-block" href="{{ route('login') }}">Connexion</a>
             @if (Route::has('register'))
-            <a href="{{ route('register') }}" class="ml-4 text-sm text-white text-decoration-none">S'inscrire</a>
+            <a class="py-2 d-none d-md-inline-block" href="{{ route('register') }}">S'inscrire</a>
             @endif
             @endauth
-        </div>
-        @endif
-        </div>
-
-
-        <nav class="container d-flex flex-column flex-md-row justify-content-between">
-    <a class="py-2" href="#" aria-label="Product">
-      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="d-block mx-auto" role="img" viewBox="0 0 24 24"><title>Product</title><circle cx="12" cy="12" r="10"/><path d="M14.31 8l5.74 9.94M9.69 8h11.48M7.38 12l5.74-9.94M9.69 16L3.95 6.06M14.31 16H2.83m13.79-4l-5.74 9.94"/></svg>
-    </a>
-    <a class="py-2 d-none d-md-inline-block" href="#">Tour</a>
-    <a class="py-2 d-none d-md-inline-block" href="#">Product</a>
-    <a class="py-2 d-none d-md-inline-block" href="#">Features</a>
-    <a class="py-2 d-none d-md-inline-block" href="#">Enterprise</a>
-    <a class="py-2 d-none d-md-inline-block" href="#">Support</a>
-    <a class="py-2 d-none d-md-inline-block" href="#">Pricing</a>
-    <a class="py-2 d-none d-md-inline-block" href="#">Cart</a>
-  </nav>
-
-
+        </nav>
     </header>
 
     <div class="mb-3">
